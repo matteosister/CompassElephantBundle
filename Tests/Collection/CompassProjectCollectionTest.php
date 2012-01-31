247b60f2673134e38b6381a6f917cf339b386c22
matteosister <matteog@gmail.com>
2012-01-31 23:51:43 +0100

    compass

---
+++ Tests/Collection/CompassProjectCollectionTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: matteo
+ * Date: 31/01/12
+ * Time: 14.37
+ *
+ * Just for fun...
+ */
+
+namespace Cypress\CompassElephantBundle\Tests\Collection;
+
+class CompassProjectCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $this->assertTrue(true);
+        $this->assertEquals(1, 1);
+    }
+}
